59e0a191260390e48ce559cd72f9cefd4ba759f5
OtabekII <otabekikromov0504@gmail.com>
2024-05-25 17:57:09 +0500

    first committt

---
+++ git.py
@@ -0,0 +1,12 @@
+"""
+firt:
+
+git init
+git add .
+git commit -m "first commit"
+
+
+
+  git config --global user.email "otabekikromov0504@gmail.com"
+  git config --global user.name "OtabekII"
+"""
